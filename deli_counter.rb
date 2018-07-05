9f62f0f9d56ffbd191363e6d6696afdbbdadc1dc
Sarah Jacob <smjacob91@gmail.com>
2018-07-05 12:54:33 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,15 @@
-katz_deli = []
-
-
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
+    announcement = "The line is currently:"
     katz_deli.each_with_index do |name, index|
-      puts "The line is currently: #{index + 1}. #{name}"
+      announcement.concat(" #{index.to_i + 1}. #{name}")
     end
+    puts announcement
   end
 end
-
-
+      
 
 def take_a_number(katz_deli, new_person)
   katz_deli.push(new_person)
